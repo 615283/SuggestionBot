9485731b5162bf5964a25a21a3fd18bbb4af305e
James <615283@users.noreply.github.com>
2018-04-12 21:31:51 +0100

    Fixed some stuff

--- src/main/java/com/georlegacy/general/suggestionbot/jda/listeners/NewResponse.java
+++ src/main/java/com/georlegacy/general/suggestionbot/jda/listeners/NewResponse.java
@@ -1,12 +1,13 @@
 package com.georlegacy.general.suggestionbot.jda.listeners;
 
 import com.georlegacy.general.suggestionbot.SuggestionBotBukkit;
-import net.dv8tion.jda.core.entities.ChannelType;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-import java.awt.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.awt.Color;
+import java.util.List;
 
 public class NewResponse extends ListenerAdapter {
     private SuggestionBotBukkit sb;
@@ -14,41 +15,38 @@ public class NewResponse extends ListenerAdapter {
         this.sb = sb;
     }
 
-    @SuppressWarnings("unused")
     @Override
-    public void onMessageReceived(MessageReceivedEvent e) {
-        if (!e.getChannel().getType().equals(ChannelType.PRIVATE)) return;
-        if (e.getAuthor().isBot()) return;
-        AtomicReference<String> content = new AtomicReference<String>();
-        e.getChannel().getHistoryBefore(e.getMessageId(), 1).queue((messageHistory) -> content.set(messageHistory.getRetrievedHistory().get(0).getEmbeds().get(0).getDescription()));
-        System.out.println(content.get());
-        if (!sb.getCanRespond().canRespond(e.getAuthor(), content.get().split("SuggestionID: ")[content.get().split("SuggestionID: ").length-1].replaceAll("`", ""))) return;
-        System.out.println(content.get());
+    public void onPrivateMessageReceived(PrivateMessageReceivedEvent e) {
+        if (!e.getAuthor().isBot())
+            e.getChannel().getHistory().retrievePast(2).queue((h) -> onMessagesRetrieved(e, h));
+    }
+
+    private void onMessagesRetrieved(PrivateMessageReceivedEvent e, List<Message> messages) {
+        User author = e.getAuthor();
+        String content = messages.get(1).getEmbeds().get(0).getDescription();
+        String[] parts = content.split("SuggestionID: ");
+        if (!sb.getCanRespond().canRespond(author, parts[parts.length-1].replaceAll("`", ""))) return;
         if (e.getMessage().getContentRaw().equalsIgnoreCase(sb.getDiscordConfig().getPrefix()+"cancel")) {
             System.out.println("is cancel");
             e.getChannel().sendMessage(sb.getEmbedUtil().buildEmbed(
                     new Color(114, 160, 255),
                     "**Response Result**",
                     "Your response to that suggestion has now been cancelled, any messages sent here will be ignored.",
-                    null,
-                    null,
-                    null,
+                    null, null, null,
                     "SuggestionsBot",
                     "https://i.imgur.com/8mLH1hi.png"))
                     .queue();
-            sb.getCanRespond().denyRespond(e.getAuthor(), content.get().split("SuggestionID: ")[content.get().split("SuggestionID: ").length-1].replaceAll("`", ""));
+            sb.getCanRespond().denyRespond(author, parts[parts.length-1].replaceAll("`", ""));
         } else {
             e.getChannel().sendMessage(sb.getEmbedUtil().buildEmbed(
                     new Color(114, 160, 255),
                     "**Response Result**",
                     "Your response has been submitted to the suggestion, any messages now sent to this channel will be ignored.",
-                    null,
-                    null,
-                    null,
+                    null, null, null,
                     "SuggestionsBot",
                     "https://i.imgur.com/8mLH1hi.png"))
                     .queue();
-            sb.getCanRespond().denyRespond(e.getAuthor(), content.get().split("SuggestionID: ")[content.get().split("SuggestionID: ").length-1]);
+            sb.getCanRespond().denyRespond(author, parts[parts.length-1]);
         }
     }
 }
